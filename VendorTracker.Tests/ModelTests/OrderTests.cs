9677785f56bf2abc6a6de4447cbec9d859b55e51
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-03-03 11:11:53 -0800

    pass order test2

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -21,5 +21,20 @@ namespace VendorTracker.Tests
   
       Assert.AreEqual(typeof(Order), testOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string name = "test name";
+      string description = "test description";
+      double price = 3.14;
+      Vendor targetVendor = new Vendor(name, description);
+      int targetVendorID = targetVendor.ID;
+
+      Order testOrder = new Order(name, description, price, targetVendorID);
+      string result = testOrder.Description;
+      
+      Assert.AreEqual(description, result);
+    }
   }
 }
