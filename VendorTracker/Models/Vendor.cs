449a9ba966047c7e4e812cd264260a902bb1ceb5
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-03-03 09:18:21 -0800

    pass vendor test 1

---
+++ VendorTracker/Models/Vendor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace VendorTracker.Models
+{
+  public class Vendor
+  {
+    // public string VendorName {get;set;}
+    // public string VendorDescription {get;set;}
+    // private static 
+  }
+}
