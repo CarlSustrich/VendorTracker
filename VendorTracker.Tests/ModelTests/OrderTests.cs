8b173b720cf2a4ec46066f541f4d92a724cfa5af
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-03-03 11:16:51 -0800

    pass order test3-5, all getters

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -23,6 +23,21 @@ namespace VendorTracker.Tests
     }
 
     [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "test name";
+      string description = "test description";
+      double price = 3.14;
+      Vendor targetVendor = new Vendor(name, description);
+      int targetVendorID = targetVendor.ID;
+
+      Order testOrder = new Order(name, description, price, targetVendorID);
+      string result = testOrder.Name;
+      
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
       string name = "test name";
@@ -33,8 +48,41 @@ namespace VendorTracker.Tests
 
       Order testOrder = new Order(name, description, price, targetVendorID);
       string result = testOrder.Description;
-      
+
       Assert.AreEqual(description, result);
     }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Double()
+    {
+      string name = "test name";
+      string description = "test description";
+      double price = 3.14;
+      Vendor targetVendor = new Vendor(name, description);
+      int targetVendorID = targetVendor.ID;
+
+      Order testOrder = new Order(name, description, price, targetVendorID);
+      double result = testOrder.Price;
+      
+      Assert.AreEqual(price, result);
+    }
+
+    [TestMethod]
+    public void GetDatePlaced_ReturnsDatePlaced_DateTime()
+    {
+      string name = "test name";
+      string description = "test description";
+      double price = 3.14;
+      Vendor targetVendor = new Vendor(name, description);
+      int targetVendorID = targetVendor.ID;
+
+      Order testOrder = new Order(name, description, price, targetVendorID);
+      DateTime result = testOrder.DatePlaced;
+      DateTime desiredResult = DateTime.Today;
+      
+      Assert.AreEqual(desiredResult, result);
+    }
+
+
   }
 }
